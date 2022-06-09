6970b2b7ae2247394c1706beafab4e6daacb7d21
davidegarigilo <s248377@studenti.polito.it>
2022-06-09 14:12:38 +0000

    Correct shapes. To adjust classifier fitting

--- model.py
+++ model.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     num_frames = 5 # I3D vs TSM
-    num_class = 174 # 8????
-    img_feature_dim = 512 # 1024
-    trm = RelationModuleMultiScaleWithClassifier(img_feature_dim, 5, num_class)
+    num_class = 8 # 8????
+    img_feature_dim = 1024 # 1024
+    trm = RelationModuleMultiScaleWithClassifier(img_feature_dim, num_frames, num_class)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -25,14 +25,21 @@ if __name__ == "__main__":
         p = pkl.load(f)
 
     train_input_feat = p['features']['RGB']
+    train_input_feat= torch.from_numpy(train_input_feat).type(torch.float32)
     train_input_ids = []
     for identifier in p['narration_ids']:
         train_input_ids.append(identifier.split('_')[-1])
      
-    
-    print(train_input_ids)
+    # extract source labels in order to fit the classifier
 
     train_labels_path = f'/content/drive/MyDrive/MLDL_2022/project/pkl_files/D{args.source}_train.pkl'
+
+    with open(train_labels_path, "rb") as f:
+        p = pkl.load(f)
+
+    df = pd.DataFrame(p)
+    labels_df = df[['uid', 'verb_class' ]]
+    print(train_input_feat.shape, type(train_input_feat))
 
     # TODO: legegre pkls test e fare join per prendere label (sia per train che per test)
     # *** TEST pkls***
@@ -51,4 +58,5 @@ if __name__ == "__main__":
     # print(train_input_ids)
 
 
-        #out = trm()
+    out = trm(train_input_feat)
+    print(out.shape)
