5aa4ca13e3be20af41401b34e38b75b60533b36e
davidegarigilo <s248377@studenti.polito.it>
2022-06-15 16:00:16 +0000

    More compact test function

--- test.py
+++ test.py
@@ -11,13 +11,11 @@ from data.dataset import PersonalizedDataset
 if __name__ == "__main__":
 
 
-    def train():
+    def train(train_loader, model, lr, epochs):
       # Define the loss
       loss_criterion = nn.CrossEntropyLoss()
       # Define the optimizer
-      lr = 0.001
-      optim = torch.optim.SGD(trm.parameters(), lr = lr, momentum = 0.9) #stochastic gradient descent
-      epochs = 30
+      optim = torch.optim.SGD(model.parameters(), lr = lr, momentum = 0.9) #stochastic gradient descent
       total_step = len(train_loader)
       train_loss = []
       for epoch in range(epochs):
@@ -30,7 +28,7 @@ if __name__ == "__main__":
 
           optim.zero_grad() #at each iteration we zero grad the gradient
 
-          out = trm(feat)
+          out = model(feat)
 
           loss = loss_criterion(out, lbl)
           # backward the error to the model
@@ -45,9 +43,21 @@ if __name__ == "__main__":
 
       print(f'\ntrain loss: {np.mean(train_loss):.4f}')
 
-    def test():
-      # TODO
-      pass
+    def test(test_loader, model):
+      
+      correct = 0
+      total = 0
+      # since we're not training, we don't need to calculate the gradients for our outputs
+      with torch.no_grad():
+        for data in test_loader:
+            feat, labels = data
+            # calculate outputs by running images through the network
+            outputs = model(feat)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+      print(100 * correct // total, '%')
     
     args = parser.parse_args()
 
@@ -82,8 +92,8 @@ if __name__ == "__main__":
       num_frames = 5 
       num_class = 8
       img_feature_dim = 2048
-      trm = RelationModuleMultiScaleWithClassifier(img_feature_dim, num_frames, num_class)
-      trm.to(device)
+      model = RelationModuleMultiScaleWithClassifier(img_feature_dim, num_frames, num_class)
+      model.to(device)
     else:
       num_frames = 5
       num_class = 8
@@ -94,7 +104,7 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_dataset, batch_size = 128, shuffle = True)
 
     # *** TRAINING ***
-    train()
+    train(train_loader = train_loader, model = model, lr = 0.001, epochs = 30)
    
 
     # *** TEST pkls***
@@ -110,33 +120,15 @@ if __name__ == "__main__":
         p = pkl.load(f)
 
     df = pd.DataFrame(p)
+
     test_labels = df['verb_class'].astype(float)
     test_labels = test_labels.to_numpy()
     test_labels = torch.from_numpy(test_labels).type(torch.LongTensor)
-    # AVG POOL not needed now since we are trying trm
-    # data = torch.mean(train_input_feat, 1)
+
     test_input_feat = test_input_feat.to(device)
     test_labels = test_labels.to(device)
     test_dataset = PersonalizedDataset(test_input_feat, test_labels)
     test_loader = DataLoader(test_dataset, batch_size = 64)
 
     # *** TESTING ***
-
-    correct = 0
-    total = 0
-    # since we're not training, we don't need to calculate the gradients for our outputs
-    with torch.no_grad():
-        for data in test_loader:
-            feat, labels = data
-            # calculate outputs by running images through the network
-            outputs = trm(feat)
-            # the class with the highest energy is what we choose as prediction
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    print(100 * correct // total, '%')
-    # print(train_input_ids)
-
-    # out = trm(train_input_feat)
-    # print(out[0], out.shape)
+    test(test_loader = test_loader, model = model)
